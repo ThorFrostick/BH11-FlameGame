aa6f93f3bfbd4394f96c04c1d59e8a6b61b17371
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2025-02-22 16:04:15 -0500

    Moved to Level 2 in separate branch

---
+++ Assets/00.Scripts/Level 2/HiddenBlock.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenBlock : MonoBehaviour
+{
+    //Get the brightness of the system screen
+    float brightness;
+
+    float transparency;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        brightness = Screen.brightness;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
