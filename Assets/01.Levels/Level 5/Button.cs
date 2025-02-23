d53750083b97ca79e47692f725b6da374f06493b
tqgiabao2006 <138303770+tqgiabao2006@users.noreply.github.com>
2025-02-22 20:03:28 -0500

    Change namespace

--- Assets/01.Levels/Level 5/Button.cs
+++ Assets/01.Levels/Level 5/Button.cs
@@ -1,71 +1,72 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public abstract class Button : MonoBehaviour
+namespace _01.Levels.Level_5
 {
-    [Header("Debug")] 
-    [SerializeField] protected bool m_isGizmos;
-    [SerializeField] protected float m_width;
-    [SerializeField] protected float m_height;
+    public abstract class Button : MonoBehaviour
+    {
+        [Header("Debug")] 
+        [SerializeField] protected bool m_isGizmos;
+        [SerializeField] protected float m_width;
+        [SerializeField] protected float m_height;
    
-    [Header("Colors")] 
-    [SerializeField] protected Color m_normalColor;
-    [SerializeField] protected Color m_selectedColor;
-    [SerializeField] protected Color m_highlightedColor;
+        [Header("Colors")] 
+        [SerializeField] protected Color m_normalColor;
+        [SerializeField] protected Color m_selectedColor;
+        [SerializeField] protected Color m_highlightedColor;
     
-    [SerializeField] protected List<GameObject> m_affectedObjects;
+        [SerializeField] protected List<GameObject> m_affectedObjects;
 
-    protected Vector2 m_mousPos;
+        protected Vector2 m_mousPos;
     
-    protected SpriteRenderer m_spriteRenderer;
+        protected SpriteRenderer m_spriteRenderer;
 
 
-    protected virtual void Start()
-    {
-        m_spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-    protected virtual void Update()
-    {
-        bool isClicked = Input.GetMouseButton(0);
-        if (isClicked && isInsideRectangle())
+        protected virtual void Start()
         {
-            m_selectedColor.a = 1;
-            m_spriteRenderer.color =  m_selectedColor;
+            m_spriteRenderer = GetComponent<SpriteRenderer>();
         }
-        else
+        protected virtual void Update()
         {
-            if (isInsideRectangle())
+            bool isClicked = Input.GetMouseButton(0);
+            if (isClicked && isInsideRectangle())
             {
-                m_highlightedColor.a = 1;
-                m_spriteRenderer.color =  m_highlightedColor;
+                m_selectedColor.a = 1;
+                m_spriteRenderer.color =  m_selectedColor;
             }
             else
             {
-                m_normalColor.a = 1;
-                m_spriteRenderer.color =  m_normalColor;
+                if (isInsideRectangle())
+                {
+                    m_highlightedColor.a = 1;
+                    m_spriteRenderer.color =  m_highlightedColor;
+                }
+                else
+                {
+                    m_normalColor.a = 1;
+                    m_spriteRenderer.color =  m_normalColor;
+                }
             }
         }
-    }
 
     
-    protected bool isInsideRectangle()
-    {
-        return
-            (m_mousPos.x > this.transform.position.x - m_width / 2
-             && m_mousPos.x < this.transform.position.x + m_width / 2
-             && m_mousPos.y > this.transform.position.y - m_height / 2
-             && m_mousPos.y < this.transform.position.y + m_height / 2
-             );
-    }
+        protected bool isInsideRectangle()
+        {
+            return
+                (m_mousPos.x > this.transform.position.x - m_width / 2
+                 && m_mousPos.x < this.transform.position.x + m_width / 2
+                 && m_mousPos.y > this.transform.position.y - m_height / 2
+                 && m_mousPos.y < this.transform.position.y + m_height / 2
+                );
+        }
 
-    #if UNITY_EDITOR
-    protected void OnDrawGizmos()
-    {
-        if (!m_isGizmos) return;
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(transform.position, new Vector2(m_width, m_height));
+#if UNITY_EDITOR
+        protected void OnDrawGizmos()
+        {
+            if (!m_isGizmos) return;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.position, new Vector2(m_width, m_height));
+        }
+#endif
     }
-    #endif
 }
