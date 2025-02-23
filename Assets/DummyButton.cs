8db086ad302053ee499fb0ff1eda0761884da924
tqgiabao2006 <138303770+tqgiabao2006@users.noreply.github.com>
2025-02-23 11:07:49 -0500

    Level_2_Finish

---
+++ Assets/DummyButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using _01.Levels.Level_5;
+using UnityEngine;
+
+public class DummyButton : Custom_Button
+{
+    public override void OnClicked()
+    {
+        
+    }
+}
